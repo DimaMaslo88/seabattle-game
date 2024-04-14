aad184b3d1caa94718f694a4db4c28b4100c1462
DimaMaslo88 <dmitrij-maslo@mail.ru>
2024-04-14 21:20:44 +0300

    'feat/mouse.js'

--- src/Mouse.js
+++ src/Mouse.js
@@ -1,12 +1,71 @@
 class Mouse {
+    element = null;
+
     under = false;
     pUnder = false;
+
     x = null;
     y = null;
+
     pX = null;
     pY = null;
 
-    constructor(element) {
+    left = false;
+    pLeft = false;
 
+    delta = 0;
+    pDelta = 0;
+
+    constructor(element) {
+this.element = element;
+element.addEventListener('mousemove', e =>{
+    this.tick()
+    this.x = e.clientX
+    this.y = e.clientY
+    this.under = true
+
+})
+element.addEventListener('mouseenter', e =>{
+    this.tick()
+    this.x = e.clientX
+    this.y = e.clientY
+    this.under = true
+
+})
+element.addEventListener('mouseleave', e =>{
+    this.tick()
+    this.x = e.clientX
+    this.y = e.clientY
+    this.under = false
+
+})
+element.addEventListener('mouseup', e =>{
+    this.tick()
+    this.x = e.clientX
+    this.y = e.clientY
+    this.under = true
+
+})
+element.addEventListener('mousedown', e =>{
+    this.tick()
+    this.x = e.clientX
+    this.y = e.clientY
+    this.under = true
+
+})
+element.addEventListener('wheel', e =>{
+    this.tick()
+    this.x = e.clientX
+    this.y = e.clientY
+    this.under = true
+
+})
+    }
+    tick(){
+        this.pX = this.x
+        this.pY = this.y
+        this.pUnder = this.under
+        this.pLeft = this.left
+        this.pDelta = this.delta
     }
 }
--- src/script.js
+++ src/script.js
@@ -1,1 +1,10 @@
-console.log('Hello')
+const mouse = new Mouse(document.body)
+
+requestAnimationFrame(tick)
+
+function tick(){
+    requestAnimationFrame(tick)
+
+    console.log(mouse.x,mouse.pX)
+    mouse.tick()
+}
